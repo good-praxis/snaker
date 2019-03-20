f18507adab80b01f1532308e24b324db996dcbbb
Praxis | Darian <hello@vulpinecat.com>
2019-03-20 14:55:27 +0100

    make tail movable

--- Snake.gd
+++ Snake.gd
@@ -4,17 +4,26 @@ enum {UP, DOWN, LEFT, RIGHT}
 
 var last_input = UP
 const BLOCKSIZE = 35
-var length = 0
+var length
 var body_part_scene = preload("res://SnakeBodyPart.tscn")
+var body_parts = []
 
 export var start_length = 2
-export var move_interval = 0.3
+export var move_interval = 2
+
 
 
 func _ready():
 	$"Move Timer".start(move_interval)
+	length = start_length
 	for i in range(start_length):
-		pass
+		var current_part = body_part_scene.instance()
+		var init_pos = position
+		init_pos.y = init_pos.y + ((i + 1) * 35)
+		current_part.init(init_pos, i)
+		$Tail.add_child(current_part)
+		current_part.set_pos()
+		body_parts.append(current_part)
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W):
@@ -27,7 +36,9 @@ func _process(delta):
 		last_input = RIGHT
 
 func _on_Move_Timer_timeout():
+	update_tail()
 	move()
+	validate_tail()
 	
 func move():
 	match last_input:
@@ -39,4 +50,14 @@ func move():
 			move_local_x(-BLOCKSIZE)
 		RIGHT:
 			move_local_x(BLOCKSIZE)
+
+func update_tail():
+	for i in range(body_parts.size()):
+		var this_part = body_parts[i]
+		this_part.tick(length, position)
+		
+func validate_tail():
+	for i in range(body_parts.size()):
+		var this_part = body_parts[i]
+		this_part.set_pos()
 
--- SnakeBodyPart.gd
+++ SnakeBodyPart.gd
@@ -4,12 +4,26 @@ var time_since_creation = 0
 var global_pos = get_global_transform()
 
 func _ready():
-	pass
+	set_pos()
 
-func init(pos):
-	print("Hello world")
+func init(pos, time=0):
 	global_pos = pos
-	
-func _process(delta):
-	set_global_transform(global_pos)
+	time_since_creation = time
 
+func set_pos():
+	set_global_transform(Transform2D(0.0, global_pos))
+
+func tick(length, pos):
+	set_pos()
+	time_since_creation += 1
+	if is_expired(length):
+		reset(pos)
+	print(is_expired(length), " ", length, " ", time_since_creation)
+
+	
+func reset(pos):
+	global_pos = pos
+	time_since_creation = 0
+
+func is_expired(length):
+	return time_since_creation >= length
